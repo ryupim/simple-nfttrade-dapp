e5828d0cb04b377c36f8f9a708a854f2b18b89f2
ryupim <shingai92@gmail.com>
2023-01-15 22:50:30 +0900

    Add solidity evnet

--- contracts/Transaction.sol
+++ contracts/Transaction.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.8.0;
 // import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract Transaction {
-    
+    event Register(uint id, string name, uint256 price);
+    event Purchase(uint id, string name, address buyer);
+    event Withdraw(address _address, uint value);
+
     mapping(uint256 => Listing) public listings;
     mapping(address => uint256) public balances;
     uint256 public ListingCount = 0;
@@ -19,9 +22,10 @@ contract Transaction {
         register("Initial Data", 100);
     }
 
-    function register(string memory name, uint256 price) public {
+    function register(string memory _name, uint256 _price) public {
         ListingCount++;
-        listings[ListingCount] = Listing(name, price, msg.sender);
+        listings[ListingCount] = Listing(_name, _price, msg.sender);
+        emit Register(ListingCount, _name, _price);
     }
 
     function purchase(uint256 idx) public payable {
@@ -30,6 +34,7 @@ contract Transaction {
         require(msg.value == item.price, "price & funds are not equal");
         balances[item.seller] += msg.value;
         item.seller = msg.sender;
+        emit Purchase(idx, item.name, msg.sender);
     }
 
     function withdraw() public {
@@ -38,6 +43,7 @@ contract Transaction {
         require(value > 0, "there is no balances");
         payable(msg.sender).transfer(value);
         balances[msg.sender] -= value;
+        emit Withdraw(msg.sender, value);
     }
 
 
